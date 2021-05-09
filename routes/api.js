dfa531f7cd8f42833c818f05fca2b2b7c2d2afcb
Dimitar Zahariev <dbzahariev@gmail.com>
2021-05-09 12:37:44 +0300

    test

--- routes/api.js
+++ routes/api.js
@@ -8,7 +8,7 @@ const BlogPost = require("../models/blogPost");
 router.get("/", (req, res) => {
   BlogPost.find({})
     .then((data) => {
-      console.log("Data: ", data);
+      // console.log("Data: ", data);
       res.json(data);
     })
     .catch((error) => {
